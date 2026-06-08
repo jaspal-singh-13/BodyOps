14ed9e8c3c1e2b4fda02c3b3fc2d88f4addaf77e
Jaspal <jasspalsinghh22@gmail.com>
2026-06-08 14:19:37 +0530

    chore: update version to 0.5.2 in main.py

--- api/main.py
+++ api/main.py
@@ -44,7 +44,7 @@ REQUIRED_ENV_VARS = [
 # so we log a warning rather than blocking startup.
 AGENT_ENV_VARS = ["AZURE_OPENAI_API_KEY", "AZURE_OPENAI_ENDPOINT", "AZURE_OPENAI_DEPLOYMENT"]
 
-VERSION = "0.5.0"
+VERSION = "0.5.2"
 
 
 @asynccontextmanager
